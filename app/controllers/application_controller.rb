f164342acb27b80a99babf36be2461a67964a63c
tpham13 <tpham0123@gmail.com>
2021-02-18 16:13:09 -0800

    Modified cors orgin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+    
+        def current_user
+            # currently a mocked version of "being logged in"
+            User.first 
+        end 
+
+        def logged_in?
+            # return a boolean rather than the current_user
+            !!current_user
+        end 
+    
 end
