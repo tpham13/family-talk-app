c1f2c5b1a165838a0838c775f60cc83eb63832f6
tpham13 <tpham0123@gmail.com>
2021-02-27 01:34:43 -0800

    Added user_id to post params

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -58,7 +58,7 @@ class Api::V1::PostsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def post_params
-      params.require(:post).permit(:content)
+      params.require(:post).permit(:content, :user_id)
     end
 
 end
